40b066a9c76d395e2f83b4d7e8fefce59d03fcac
Ozumah Ebenezer <ozumahe@gmail.com>
2023-05-18 21:22:31 +0100

    Fix: Picture NextJS Error

--- src/components/Dashboard/SideBar.tsx
+++ src/components/Dashboard/SideBar.tsx
@@ -3,9 +3,9 @@ import Tab from "./Tab";
 function SideBar() {
   return (
     <div className="fixed top-0 left-0 bottom-0 w-[282px] shadow-sidenav py-[30px] px-[20px]">
-      <picture>
+      <div>
         <img className="mx-auto" src="/imgs/logo-large.svg" alt="logo" />
-      </picture>
+      </div>
       <div className="w-full h-0 border-[1px] border-[#D7D7D7] mt-[20px]" />
       <div className="pt-[40px] px-[5px]">
         <p className="text-[12px] font-normal text-[#6C6C6C]">MENU</p>
