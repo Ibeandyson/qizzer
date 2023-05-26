dcdb1e0eeeee96f2d706943b851603e674c5c388
Ozumah Ebenezer <ozumahe@gmail.com>
2023-05-26 15:41:40 +0100

    Fix: Next Error Fixed

--- src/app/account-verification/page.tsx
+++ src/app/account-verification/page.tsx
@@ -13,12 +13,13 @@ import {
 
 export default function AccountVerification() {
   const [password, setPassword] = useState("");
-  const handleSubmit = (e) => {
+  const handleSubmit = (e: any) => {
     e.preventDefault();
     // Handle submit logic with the password value
     console.log(password);
   };
-  const handleChange = (e, index) => {
+
+  const handleChange = (e: any, index: any) => {
     const newPassword = e.target.value;
     setPassword((prevPassword) => {
       const updatedPassword = prevPassword.split("");
@@ -27,79 +28,90 @@ export default function AccountVerification() {
     });
   };
   return (
-    <Box
-      height="100vh"
-      width="100vw"
-      display="flex"
-      flexDir="column"
-      alignItems="center"
-      padding={{ base: "1rem", md: "5rem" }}
-      py={{ base: "5rem" }}
-      backgroundImage={`url(../images/AccountverificationBg.svg)`}
-      backgroundSize="cover"
-      backgroundPosition="start"
-      overflowX="hidden"
-    >
-      <Box display="flex" alignItems="center" mb="1rem">
-        <Image width="39px" src="../images/logo.svg" />
-        <Text fontSize="24px" fontWeight="500" color="#220075">
-          TrustPaddi
-        </Text>
-      </Box>
+    <chakra.main>
       <Box
-        backgroundColor="white"
-        width={{ base: "100%", md: "50%" }}
-        minH="50%"
-        py={{ base: "2rem", md: "5rem" }}
-        px={{ base: "2rem", md: "2rem" }}
-        textAlign="center"
-        borderRadius="8px"
-        boxShadow="md"
+        height="100vh"
+        width="100vw"
+        display="flex"
+        flexDir="column"
+        alignItems="center"
+        padding={{ base: "1rem", md: "5rem" }}
+        py={{ base: "5rem" }}
+        backgroundImage={`url(../images/AccountverificationBg.svg)`}
+        backgroundSize="cover"
+        backgroundPosition="start"
+        overflowX="hidden"
       >
-        <Text fontSize={{ base: "16px", md: "32px" }} fontWeight="500" color="#1E1E1E">
-          Email Verification
-        </Text>
-        <Text
-          fontSize={{ base: "10px", md: "14px" }}
-          fontWeight="400"
+        <Box display="flex" alignItems="center" mb="1rem">
+          <Image width="39px" src="../images/logo.svg" />
+          <Text fontSize="24px" fontWeight="500" color="#220075">
+            TrustPaddi
+          </Text>
+        </Box>
+        <Box
+          backgroundColor="white"
+          width={{ base: "100%", md: "50%" }}
+          minH="50%"
+          py={{ base: "2rem", md: "5rem" }}
+          px={{ base: "2rem", md: "2rem" }}
           textAlign="center"
-          lineHeight="16.8px"
-          color="#333333"
+          borderRadius="8px"
+          boxShadow="md"
         >
-          Enter the verification code we sent to new_user@email.com.
-          <span style={{ color: "purple", fontWeight: "500" }}>
-            Resend code
-          </span>
-        </Text>
-        <chakra.form onSubmit={handleSubmit}>
-          <Stack spacing={{ base: "2", md: "4" }} direction="row" align="center" justify="center">
-            {[...Array(6)].map((_, index) => (
-              <Input
-                key="index"
-                my="1rem"
-                boder="1px solid black"
-                type="password"
-                width={{ base: "1rem", md: "2.5rem" }}
-                size="lg"
-                value={password[index] || ""}
-                onChange={(e) => handleChange(e, index)}
-              />
-            ))}
-          </Stack>
-          <Button
-            type="submit"
-            height="55px"
-            width="100%"
-            left="0px"
-            borderRadius="8px"
-            padding="12px 48px"
-            color="white"
-            backgroundColor="#220075"
+          <Text
+            fontSize={{ base: "16px", md: "32px" }}
+            fontWeight="500"
+            color="#1E1E1E"
           >
-            Continue
-          </Button>
-        </chakra.form>
+            Email Verification
+          </Text>
+          <Text
+            fontSize={{ base: "10px", md: "14px" }}
+            fontWeight="400"
+            textAlign="center"
+            lineHeight="16.8px"
+            color="#333333"
+          >
+            Enter the verification code we sent to new_user@email.com.
+            <span style={{ color: "purple", fontWeight: "500" }}>
+              Resend code
+            </span>
+          </Text>
+          <chakra.form onSubmit={handleSubmit}>
+            <Stack
+              spacing={{ base: "2", md: "4" }}
+              direction="row"
+              align="center"
+              justify="center"
+            >
+              {[...Array(6)].map((_, index) => (
+                <Input
+                  key="index"
+                  my="1rem"
+                  border="1px solid black"
+                  type="password"
+                  width={{ base: "1rem", md: "2.5rem" }}
+                  size="lg"
+                  value={password[index] || ""}
+                  onChange={(e) => handleChange(e, index)}
+                />
+              ))}
+            </Stack>
+            <Button
+              type="submit"
+              height="55px"
+              width="100%"
+              left="0px"
+              borderRadius="8px"
+              padding="12px 48px"
+              color="white"
+              backgroundColor="#220075"
+            >
+              Continue
+            </Button>
+          </chakra.form>
+        </Box>
       </Box>
-    </Box>
+    </chakra.main>
   );
 }
