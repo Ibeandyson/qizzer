fac54c113c7269456f6b72bc9916d123bdf71641
Ozumah Ebenezer <ozumahe@gmail.com>
2023-05-29 13:38:10 +0100

    Fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 "use client";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+// import { useRouter } from "next/navigation";
+// import { useEffect } from "react";
 
 export default function Home() {
-  const router = useRouter();
+  // const router = useRouter();
 
-  useEffect(() => {
-    router.push("/dashboard");
-  }, []);
+  // useEffect(() => {
+  //   router.push("/dashboard");
+  // }, []);
 
   return (
     <main>
