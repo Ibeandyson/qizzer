e017e49a43cdc4369f1e7487e9ec83d2b24416ca
Ozumah Ebenezer <ozumahe@gmail.com>
2023-06-17 22:20:42 +0100

    UPDTE

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,15 +2,23 @@
 import React from "react";
 import { chakra, useDisclosure } from "@chakra-ui/react";
 import SampleModal from "../../components/ReuseableComponents/Modals/SampleModal";
+import SampleBottomSheet from "@/components/ReuseableComponents/BottomSheet/SampleBottoSheet";
 // import SampleModal from "@/components/ReuseableComponents/Modals/SampleModal";
 
 export default function Dashboard() {
   const { isOpen, onOpen, onClose }: any = useDisclosure();
+  const sheet: any = useDisclosure();
 
   return (
     <chakra.div>
       <chakra.button onClick={() => onOpen()}>OPmen</chakra.button>
+      <chakra.button onClick={() => sheet.onOpen()}>OPen Sheet </chakra.button>
+
       <SampleModal isOpen={isOpen} onClose={onClose} />
+      <SampleBottomSheet
+        isOpen={sheet.isOpen}
+        onClose={() => sheet.onClose()}
+      />
     </chakra.div>
   );
 }
