03420cd0d88ee65611f3a6ce22b182fa6c830487
Ibeandyson <ibeandyson123@gmail.com>
2023-06-19 10:27:40 +0100

    made on fix on linter

--- src/global/hooks/useUser.ts
+++ src/global/hooks/useUser.ts
@@ -1,16 +1,17 @@
+import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from "../appState/store"
 import { setToke } from "../appState/slice/userSlice";
-import { useSelector, useDispatch } from 'react-redux';
+
 
 const useUser = () => {
-    const state = useSelector((state: RootState) => state.user.token)
+    const userstate = useSelector((state: RootState) => state.user.token)
     const dispatch = useDispatch()
     const setToken = () => {
         dispatch(setToke("1000"))
     }
 
     return {
-        token: state,
+        token: userstate,
         setToken
     }
 }
