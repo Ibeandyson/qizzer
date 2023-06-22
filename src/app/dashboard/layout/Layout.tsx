ab1190bb9db48f07af0e5458b4f2b26a6aa428ac
Ozumah Ebenezer <ozumahe@gmail.com>
2023-06-22 02:14:56 +0100

    FIXES

--- src/app/dashboard/layout/Layout.tsx
+++ src/app/dashboard/layout/Layout.tsx
@@ -1,35 +1,32 @@
-'use client';
+"use client";
 
-import {chakra, useDisclosure, Container} from '@chakra-ui/react';
-import {ReactNode} from 'react';
-import SideBar from '../../../components/Dashboard/SideBar';
-import NavBar from '../../../components/Dashboard/NavBar';
+import { chakra, useDisclosure, Container } from "@chakra-ui/react";
+import { ReactNode } from "react";
+import SideBar from "../../../components/Dashboard/SideBar";
+import NavBar from "../../../components/Dashboard/NavBar";
 
 type Props = {
   children: ReactNode;
 };
 
-function Layout({children}: Props) {
-  const {isOpen, onOpen, onClose} = useDisclosure();
+function Layout({ children }: Props) {
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
     <chakra.main>
       <SideBar isOpen={isOpen} onClose={onClose} />
-      <chakra.div pl={{lg: '200px'}}>
+      <chakra.div pl={{ lg: "200px" }}>
         <NavBar onOpen={onOpen} />
-        <Container maxW="1050px">
-          <chakra.div
-            bg="white"
-            ml={{base: '0', lg: '10px'}}
-            p={{base: '5px', lg: '20px'}}
-            boxShadow={{base: 'none', md: 'md'}}
-            width="100%"
-            height="75vh"
-            mt="30px"
-            zIndex="1">
-            {children}
-          </chakra.div>
-        </Container>
+        <chakra.div
+          bg="white"
+          ml={{ base: "0", lg: "10px" }}
+          p={{ base: "5px", lg: "40px" }}
+          width="100%"
+          height="75vh"
+          zIndex="1"
+        >
+          {children}
+        </chakra.div>
       </chakra.div>
     </chakra.main>
   );
