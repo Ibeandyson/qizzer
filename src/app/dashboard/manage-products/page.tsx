43e6367964d57989301614ba3e5fc1cfb552423c
Ozumah Ebenezer <ozumahe@gmail.com>
2023-07-16 22:51:43 +0100

    Manage Store Update

--- src/app/dashboard/manage-products/page.tsx
+++ src/app/dashboard/manage-products/page.tsx
@@ -2,10 +2,11 @@
 
 import React, { useState } from "react";
 
-import { chakra } from "@chakra-ui/react";
+import { Select, chakra } from "@chakra-ui/react";
 import ButtonUI from "@/components/ReuseableComponents/ButtonUI";
 import PlusIcon from "@/components/SVG-Icons/PlusIcon";
 import InputUI from "@/components/ReuseableComponents/InputUI";
+import CreateProduct from "@/components/Store/ManageProducts/CreateProduct";
 type Props = {};
 
 function ManageProducts({}: Props) {
@@ -39,7 +40,7 @@ function ManageProducts({}: Props) {
             fontWeight="500"
             color="#000000"
           >
-            {showProductsForm ? "Create New Store" : "Manage Products"}
+            {showProductsForm ? "Create New Product" : "Manage Products"}
           </chakra.p>
         </chakra.div>
         <chakra.button
@@ -53,117 +54,53 @@ function ManageProducts({}: Props) {
           gap="10px"
         ></chakra.button>
       </chakra.div>
-      {!showProductsForm ? (
-        <chakra.div
-          w="full"
-          h="500px"
-          borderRadius="4px"
-          border="2px dashed #7A66AC"
-          display="flex"
-          justifyContent="center"
-          alignItems="center"
-          gap="15px"
-          flexDir="column"
-        >
-          <chakra.img src="/imgs/cuate.png" w="250px" />
-          <chakra.p
-            fontSize={{ base: "14px", lg: "20px" }}
-            fontWeight="500"
-            color="#1E1E1E"
-          >
-            No product added yet.
-          </chakra.p>
-          <chakra.p
-            fontSize={{
-              base: "12px",
-              lg: "14px",
-            }}
-            fontWeight="400"
-            color="#747474"
-            mb="20px"
-          >
-            Please click the button below to add a product to your store.
-          </chakra.p>
-          <ButtonUI
-            leftIcon={<PlusIcon />}
-            label="Add New Product"
-            loading={false}
-            onClick={() => setShowProductsForm(true)}
-            w="200px"
-          />
-        </chakra.div>
+      {Array(20).fill("").length < 1 ? (
+        <>
+          {!showProductsForm ? (
+            <chakra.div
+              w="full"
+              h="500px"
+              borderRadius="4px"
+              border="2px dashed #7A66AC"
+              display="flex"
+              justifyContent="center"
+              alignItems="center"
+              gap="15px"
+              flexDir="column"
+            >
+              <chakra.img src="/imgs/cuate.png" w="250px" />
+              <chakra.p
+                fontSize={{ base: "14px", lg: "20px" }}
+                fontWeight="500"
+                color="#1E1E1E"
+              >
+                No product added yet.
+              </chakra.p>
+              <chakra.p
+                fontSize={{
+                  base: "12px",
+                  lg: "14px",
+                }}
+                fontWeight="400"
+                color="#747474"
+                mb="20px"
+              >
+                Please click the button below to add a product to your store.
+              </chakra.p>
+              <ButtonUI
+                leftIcon={<PlusIcon />}
+                label="Add New Product"
+                loading={false}
+                onClick={() => setShowProductsForm(true)}
+                w="200px"
+              />
+            </chakra.div>
+          ) : (
+            <CreateProduct />
+          )}
+        </>
       ) : (
-        <chakra.div>
-          <chakra.div
-            bg="#D9D9D9"
-            h="100px"
-            w="100px"
-            mx="auto"
-            borderRadius="full"
-            display="flex"
-            justifyContent="center"
-            alignItems="center"
-            flexDir="column"
-          >
-            <chakra.img
-              src="/icons/ic_round-cloud-upload.svg"
-              alt="ic_round-cloud-upload"
-            />
-            <chakra.p fontSize="10px">Upload Logo</chakra.p>
-          </chakra.div>
-          <chakra.div
-            mt="30px"
-            display="grid"
-            gap="20px"
-            gridTemplateColumns={{ lg: "repeat(2, 1fr)" }}
-          >
-            <InputUI
-              value={""}
-              type="text"
-              lable="Store Name *"
-              name="StoreName"
-              onchange={() => {}}
-            />
-            <InputUI
-              value={""}
-              type="text"
-              lable="Shop address *"
-              name="ShopAddress"
-              onchange={() => {}}
-            />{" "}
-            <InputUI
-              value={""}
-              type="text"
-              lable="City *"
-              name="City"
-              onchange={() => {}}
-            />{" "}
-            <InputUI
-              value={""}
-              type="text"
-              lable="State *"
-              name="State"
-              onchange={() => {}}
-            />{" "}
-            <InputUI
-              value={""}
-              type="text"
-              lable="Postal Code"
-              name="Postal Code"
-              onchange={() => {}}
-            />{" "}
-            <InputUI
-              value={""}
-              type="text"
-              lable="Account manager phone number *"
-              name="phoneNumber"
-              onchange={() => {}}
-            />
-          </chakra.div>
-          <chakra.div display="flex" justifyContent="center" my="30px">
-            <ButtonUI label="Create Store" w="430px" onClick={() => {}} />
-          </chakra.div>
-        </chakra.div>
+        <></>
       )}
     </chakra.div>
   );
--- src/app/dashboard/manage-store/page.tsx
+++ src/app/dashboard/manage-store/page.tsx
@@ -6,6 +6,7 @@ import { Select, chakra } from "@chakra-ui/react";
 import ButtonUI from "@/components/ReuseableComponents/ButtonUI";
 import PlusIcon from "@/components/SVG-Icons/PlusIcon";
 import InputUI from "@/components/ReuseableComponents/InputUI";
+import CreateStore from "@/components/Store/ManageStore/CreateStore";
 type Props = {};
 
 function ManageStore({}: Props) {
@@ -53,139 +54,50 @@ function ManageStore({}: Props) {
           gap="10px"
         ></chakra.button>
       </chakra.div>
-      {!showStoreForm ? (
-        <chakra.div
-          w="full"
-          h="500px"
-          borderRadius="4px"
-          border="2px dashed #7A66AC"
-          display="flex"
-          justifyContent="center"
-          alignItems="center"
-          gap="15px"
-          flexDir="column"
-        >
-          <chakra.img src="/imgs/cuate.png" w="250px" />
-          <chakra.p
-            fontSize={{ base: "14px", lg: "20px" }}
-            fontWeight="500"
-            color="#1E1E1E"
-          >
-            No store created yet.
-          </chakra.p>
-          <chakra.p
-            fontSize={{
-              base: "12px",
-              lg: "14px",
-            }}
-            fontWeight="400"
-            color="#747474"
-            mb="20px"
-          >
-            Please click the button below to create a store as a seller.
-          </chakra.p>
-          <ButtonUI
-            leftIcon={<PlusIcon />}
-            label="Create New Store"
-            loading={false}
-            onClick={() => setShowStoreForm(true)}
-            w="200px"
-          />
-        </chakra.div>
-      ) : (
-        <chakra.div>
+      <>
+        {!showStoreForm ? (
           <chakra.div
-            mt="30px"
-            display="grid"
-            gap="20px"
-            gridTemplateColumns={{ lg: "repeat(2, 1fr)" }}
-          >
-            <InputUI
-              value={""}
-              type="text"
-              lable="Product Name *"
-              name="ProductName"
-              onchange={() => {}}
-            />
-            <chakra.div>
-              <chakra.p
-                color="#1E1E1E"
-                fontWeight="500"
-                fontSize={{ base: "14px", lg: "16px" }}
-              >
-                Category *
-              </chakra.p>
-              <Select h="50px" mt="8px" placeholder="Select Category">
-                <option value=""></option>
-                <option value=""></option>
-                <option value=""></option>
-                <option value=""></option>
-                <option value=""></option>
-              </Select>
-            </chakra.div>
-          </chakra.div>
-          <chakra.div
-            mt="30px"
-            display="grid"
-            gap="20px"
-            gridTemplateColumns={{ lg: "repeat(2, 1fr)" }}
-          >
-            <InputUI
-              value={""}
-              type="text"
-              lable="Price (NGN) *"
-              name="price"
-              onchange={() => {}}
-            />{" "}
-            <InputUI
-              value={""}
-              type="text"
-              lable="Quantity *"
-              name="quantity"
-              onchange={() => {}}
-            />{" "}
-            <InputUI
-              value={""}
-              type="text"
-              lable="Address *"
-              name="Address"
-              onchange={() => {}}
-            />{" "}
-            <InputUI
-              value={""}
-              type="text"
-              lable="City *"
-              name="city"
-              onchange={() => {}}
-            />{" "}
-            <InputUI
-              value={""}
-              type="text"
-              lable="State *"
-              name="state"
-              onchange={() => {}}
-            />
-            <InputUI
-              value={""}
-              type="text"
-              lable="Phone Number *"
-              name="PhoneNumber"
-              onchange={() => {}}
-            />
-          </chakra.div>
-          <chakra.div
+            w="full"
+            h="500px"
+            borderRadius="4px"
+            border="2px dashed #7A66AC"
             display="flex"
-            justifyContent="end"
-            gap="30px"
-            my="30px"
-          ></chakra.div>
-          <chakra.div display="flex" justifyContent="end" gap="30px" my="30px">
-            <ButtonUI label="Save As Draft" w="163px" onClick={() => {}} />
-
-            <ButtonUI label="Publish Product" w="163px" onClick={() => {}} />
+            justifyContent="center"
+            alignItems="center"
+            gap="15px"
+            flexDir="column"
+          >
+            <chakra.img src="/imgs/cuate.png" w="250px" />
+            <chakra.p
+              fontSize={{ base: "14px", lg: "20px" }}
+              fontWeight="500"
+              color="#1E1E1E"
+            >
+              No store created yet.
+            </chakra.p>
+            <chakra.p
+              fontSize={{
+                base: "12px",
+                lg: "14px",
+              }}
+              fontWeight="400"
+              color="#747474"
+              mb="20px"
+            >
+              Please click the button below to create a store as a seller.
+            </chakra.p>
+            <ButtonUI
+              leftIcon={<PlusIcon />}
+              label="Create New Store"
+              loading={false}
+              onClick={() => setShowStoreForm(true)}
+              w="200px"
+            />
           </chakra.div>
-        </chakra.div>
-      )}
+        ) : (
+          <CreateStore />
+        )}
+      </>
     </chakra.div>
   );
 }
