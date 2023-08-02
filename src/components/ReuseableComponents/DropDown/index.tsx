1e0519fdd6850730d6072edfa1c5e540dd0c1346
Ozumah Ebenezer <ozumahe@gmail.com>
2023-08-02 14:08:25 +0100

    cp: ActionDropDown Added

--- src/components/ReuseableComponents/DropDown/index.tsx
+++ src/components/ReuseableComponents/DropDown/index.tsx
@@ -1,23 +1,58 @@
-import React from "react";
-import { Menu, MenuButton, MenuList, MenuItem } from "@chakra-ui/react";
-type Props = {};
+import React, { ReactNode } from "react";
+import { Menu, MenuButton, MenuList, MenuItem, Button } from "@chakra-ui/react";
+import DropDownIcon from "@/components/SVG-Icons/DropDownIcon";
 
-function DropDown({}: Props) {
+export type ActionProps = {
+  label: string;
+  onAction: () => void;
+  color?: string;
+  Icon: any;
+};
+
+type Props = {
+  actions: ActionProps[];
+};
+
+function ActionsDropDown({ actions }: Props) {
   return (
     <Menu>
-      <MenuButton>
-        {/* <MenuButton as={Button} rightIcon={}> */}
-        Actions
-      </MenuButton>
-      <MenuList>
-        <MenuItem>Download</MenuItem>
-        <MenuItem>Create a Copy</MenuItem>
-        <MenuItem>Mark as Draft</MenuItem>
-        <MenuItem>Delete</MenuItem>
-        <MenuItem>Attend a Workshop</MenuItem>
-      </MenuList>
+      {({ isOpen }) => (
+        <>
+          <MenuButton
+            bg="#220075"
+            w={{ base: "72.5px", lg: "119px" }}
+            h={{ base: "25.06px", lg: "41px" }}
+            borderRadius={{ base: "5px", lg: "8px" }}
+            color="#fff"
+            fontSize={{ base: "9.778px", lg: "16px" }}
+            px={{ base: "10px", lg: "20px" }}
+            fontWeight={500}
+            as={Button}
+            rightIcon={<DropDownIcon rotate={isOpen} color="#FFF" />}
+            _hover={{}}
+            _active={{}}
+          >
+            Actions
+          </MenuButton>
+          <MenuList w={{ base: "110.17px", lg: "179px" }}>
+            {actions.map(({ label, onAction, color, Icon }, index) => (
+              <MenuItem
+                key={index}
+                as={Button}
+                leftIcon={Icon}
+                onClick={onAction}
+                color={color ? color : "#1E1E1E"}
+                w={{ base: "110.17px", lg: "179px" }}
+                h={{ base: "24.78px", lg: "41px" }}
+              >
+                {label}
+              </MenuItem>
+            ))}
+          </MenuList>
+        </>
+      )}
     </Menu>
   );
 }
 
-export default DropDown;
+export default ActionsDropDown;
