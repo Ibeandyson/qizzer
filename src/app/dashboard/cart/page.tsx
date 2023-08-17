297f607e63d7e11f38670c734bee4c10e2ac8391
Ibeandyson <ibeandyson123@gmail.com>
2023-08-18 00:03:12 +0100

    cart

---
+++ src/app/dashboard/cart/page.tsx
@@ -0,0 +1,72 @@
+'use client';
+import { chakra, Stepper, Step, StepIndicator, StepStatus, Box, Button, useSteps, Text, StepIcon, StepSeparator, Flex } from '@chakra-ui/react';
+import { CartList, DeliveryInfoForm } from '@/components/Cart';
+import ButtonUI from '@/components/ReuseableComponents/ButtonUI';
+
+
+const steps = [
+  { title: 'Products In Cart', id: 1 },
+  { title: 'Delivery Details', id: 2 },
+  { title: 'Payment', id: 3 },
+];
+
+export default function Cart({ params }: { params: { id: string } }) {
+  const { activeStep, setActiveStep } = useSteps({
+    index: 1,
+    count: steps.length,
+  });
+
+  return (
+    <>
+      <Box position="relative">
+        <Stepper size="sm" index={activeStep} gap="0">
+          {steps.map((step, index) => (
+            <Step key={index}>
+              <StepIndicator cursor="pointer" bg="white" onClick={() => setActiveStep(step.id)}>
+                <StepStatus complete={<StepIcon />} />
+              </StepIndicator>
+              <StepSeparator />
+            </Step>
+          ))}
+        </Stepper>
+        <Flex justifyContent="space-between">
+          {steps.map((data) => (
+            <Text mt="15px" fontSize="10px" fontWeight="600">
+              {data.title}
+            </Text>
+          ))}
+        </Flex>
+      </Box>
+      {activeStep == 1 && (
+        <chakra.div shadow={{ base: 'none', md: 'sm' }} mt="50px">
+          <CartList />
+          <Box mt={{ base: '10%', md: '5%' }} display="flex">
+            <Button mb="5%" variant="outline" borderColor="0.5px #333333" color="#333333" fontSize="15px" fontWeight="500">
+              Clear Caert
+            </Button>
+            <Box ml="auto">
+              <Button
+                onClick={() => setActiveStep(2)}
+                _hover={{ backgroundColor: '#220075' }}
+                ml="20px"
+                mb="5%"
+                variant="outline"
+                bgColor="#220075"
+                color="#FFFFFF"
+                fontSize="15px"
+                fontWeight="500"
+              >
+                Checkout
+              </Button>
+            </Box>
+          </Box>
+        </chakra.div>
+      )}
+      {activeStep == 2 && (
+        <chakra.div>
+          <DeliveryInfoForm />
+        </chakra.div>
+      )}
+    </>
+  );
+}
--- src/components/ReuseableComponents/SelectUI/index.tsx
+++ src/components/ReuseableComponents/SelectUI/index.tsx
@@ -1,4 +1,5 @@
-import { Input, Text } from '@chakra-ui/react';
+'use client';
+import { Select, Text } from '@chakra-ui/react';
 import React from 'react';
 
 type Props = {
@@ -6,33 +7,32 @@ type Props = {
   disable?: boolean;
   onchange: (e: any) => void;
   name?: string;
-  value: string | number;
   lable?: string;
   style?: React.CSSProperties;
-  type?: string;
+  children: React.ReactNode;
 };
 
-const InputUI = ({ type, placeHolder, onchange, disable, value, name, lable, style }: Props) => {
+const SelectUI = ({ placeHolder, onchange, disable, name, lable, style, children }: Props) => {
   return (
     <div style={style}>
       <Text textAlign="start" color="#1E1E1E" fontWeight="500" fontSize={{ base: '14px', lg: '16px' }} mb="8px">
         {lable}
       </Text>
-      <Input
-        type={type}
-        value={value}
-        name={name}
-        disabled={disable}
-        placeholder={placeHolder}
-        onChange={onchange}
+      <Select
         w={{ base: 'full' }}
         h={{ base: '40px', lg: '51px' }}
         borderColor="#7A7A7A"
         border="solid 1px"
         _focus={{ borderColor: '#220075' }}
-      />
+        name={name}
+        disabled={disable}
+        onChange={onchange}
+        placeholder={placeHolder}
+      >
+        {children}
+      </Select>
     </div>
   );
 };
 
-export default InputUI;
+export default SelectUI;
